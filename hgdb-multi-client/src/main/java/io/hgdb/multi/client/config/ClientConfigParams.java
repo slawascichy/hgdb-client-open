ae582e776287becdc2e1717fb948352a51ddba69
Sławomir 'slawas' Cichy <scichy@scisoftware.pl>
2022-07-05 09:56:57 +0200

    Usunąłem niepotrzebny parametr konfiguracyjny.

--- hgdb-multi-client/src/main/java/io/hgdb/multi/client/config/ClientConfigParams.java
+++ hgdb-multi-client/src/main/java/io/hgdb/multi/client/config/ClientConfigParams.java
@@ -6,8 +6,7 @@ import pro.ibpm.mercury.config.MercuryConfig;
 
 /**
  * 
- * ClientConfigParams zbiór parametrów oraz wsparcie związane z definiowaniem
- * komunikacji z serwerami bazy danych.
+ * ClientConfigParams zbiór parametrów oraz wsparcie związane z definiowaniem komunikacji z serwerami bazy danych.
  *
  * @author Sławomir Cichy &lt;slawas@scisoftware.pl&gt;
  * @version $Revision: 1.1 $
@@ -21,13 +20,15 @@ public class ClientConfigParams {
 	public static final String WS_DEFAULT_IDENTIFIER = "cxf";
 	public static final String WS_DEFAULT_TOKEN = "secret";
 
-	public static final String RMI_DEFAULT_SERVICE_PROP = "mercury.rmi.service.default";
+	public static final String RMI_SERVICE_DEFAULT_PROP = "mercury.rmi.service.default";
+	public static final String RMI_SERVICE_LIST_PROP = "mercury.rmi.service.list";
 	public static final String RMI_DEFAULT_SERVICE = "localhost";
 	public static final String RMI_REMOTE_SERVICE_URL_PROP = "%s.rmi.url";
 	public static final String RMI_SECURITY_IDENTIFIER_PROP = "%s.rmi.security.identifier";
 	public static final String RMI_SECURITY_TOKEN_PROP = "%s.rmi.security.token";
 
-	public static final String WS_DEFAULT_SERVICE_PROP = "mercury.ws.service.default";
+	public static final String WS_SERVICE_DEFAULT_PROP = "mercury.ws.service.default";
+	public static final String WS_SERVICE_LIST_PROP = "mercury.ws.service.list";
 	public static final String WS_DEFAULT_SERVICE = "localhost";
 	public static final String WS_REMOTE_SERVICE_URL_PROP = "%s.ws.url";
 	public static final String WS_SECURITY_IDENTIFIER_PROP = "%s.ws.security.identifier";
@@ -35,38 +36,30 @@ public class ClientConfigParams {
 	public static final String WS_REMOTE_SECURITY_ENABLED_PROP = "%s.ws.security.enabled";
 
 	/**
-	 * Parametr definiujący maksymalną liczbę połączeń w puli połączeń
-	 * wykorzystywanych do przekierowania ruchu.
+	 * Parametr definiujący maksymalną liczbę połączeń w puli połączeń wykorzystywanych do przekierowania ruchu.
 	 */
 	public static final String CONNECTION_MAX_CONNECTIONS_PROP = "mercury.connection.max.connections.total";
 	private static Integer connectionMaxConnections;
 
 	/**
-	 * Parametr definiujący maksymalną liczbę połączeń dla konkretnej usługi w puli
-	 * połączeń wykorzystywanych do przekierowania ruchu.
+	 * Parametr definiujący maksymalną liczbę połączeń dla konkretnej usługi w puli połączeń wykorzystywanych do
+	 * przekierowania ruchu.
 	 */
 	public static final String CONNECTION_MAX_CONNECTIONS_PER_ROUTE_PROP = "mercury.connection.max.connections.perRoute";
 	private static Integer connectionMaxConnectionsPerRoute;
 
 	/**
-	 * Parametr definiujący timeout odczytu dla połączeń z instancjami bazy mercury
-	 * wykorzystywanych do przekierowania ruchu. Wartość wyrażona w milisekundach.
+	 * Parametr definiujący timeout odczytu dla połączeń z instancjami bazy mercury wykorzystywanych do przekierowania
+	 * ruchu. Wartość wyrażona w milisekundach.
 	 */
 	public static final String CONNECTION_READ_TIMEOUT_PROP = "mercury.connection.read.timeout";
 	private static Integer connectionReadTimeout;
 
 	/**
-	 * Parametr definiujący timeout dla połączenia w puli. Po tym czasie, nieaktywne
-	 * połączenie zostanie zamknięte.
+	 * Parametr definiujący timeout dla połączenia w puli. Po tym czasie, nieaktywne połączenie zostanie zamknięte.
 	 */
 	public static final String CONNECTION_CONNECTION_TIMEOUT_PROP = "mercury.connection.connection.timeout";
 	private static Integer connectionConnectionTimeout;
-
-	/**
-	 * Parametr definiujący timeout podczas pobierania połączenia z puli połączeń.
-	 */
-	public static final String CONNECTION_CONNECTION_REQUEST_TIMEOUT_PROP = "mercury.connection.connection.request.timeout";
-	private static Integer connectionConnectionRequestTimeout;
 
 	public static final int DEFAULT_MAX_TOTAL_CONNECTIONS = 256;
 	public static final int DEFAULT_MAX_CONNECTIONS_PER_ROUTE = 128;
@@ -130,21 +123,6 @@ public class ClientConfigParams {
 			}
 		}
 		return connectionConnectionTimeout;
-	}
-
-	/**
-	 * @return the {@link #connectionConnectionRequestTimeout}
-	 */
-	public static int getConnectionConnectionRequestTimeout() {
-		if (connectionConnectionRequestTimeout == null) {
-			String propValue = MercuryConfig.getInstance().get(CONNECTION_CONNECTION_REQUEST_TIMEOUT_PROP);
-			if (StringUtils.isBlank(propValue)) {
-				connectionConnectionRequestTimeout = 0;
-			} else {
-				connectionConnectionRequestTimeout = Integer.parseInt(propValue);
-			}
-		}
-		return connectionConnectionRequestTimeout;
 	}
 
 }
